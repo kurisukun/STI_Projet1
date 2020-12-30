5e397d5965669d2e6f0cf9326ac22d647b49952f
Gil Balsiger <gil.balsiger@gmail.com>
2020-12-30 16:03:26 +0100

    refactor(database): list messages from MySQL database

--- site/html/list_messages.php
+++ site/html/list_messages.php
@@ -1,57 +1,74 @@
 <?php
-    ob_start();
-    session_start();
-    include("header.php");
-    include("redirect.php");
-    //On s'assure que les données mises dans une session précédente soient effacées
-    unset($_SESSION['retitle']);
-    unset($_SESSION['receiver']);
+
+use App\Database;
+
+require 'includes.php';
+include("redirect.php");
+
+//On s'assure que les données mises dans une session précédente soient effacées
+unset($_SESSION['retitle']);
+unset($_SESSION['receiver']);
+
+$pdo = Database::getInstance()->getPdo();
+
+// Vérifie que le bouton delet a bien été pressé
+if (isset($_POST['delete'])) {
+    // Et que l'id du message existe bien
+    if (isset($_POST['messageid'])) {
+        $message_id = $_POST['messageid'];
+        $pdo->exec(" DELETE FROM messages WHERE id=$message_id; ");
+        header('Location: ' . $_SERVER['REQUEST_URI']);
+        die();
+    }
+} //Sinon on regarde si c'est le bouton answer
+else if (isset($_POST['answer'])) {
+    // On continue seulement si l'id du message et le titre son bien set
+    if (isset($_POST['messageid']) && isset($_POST['messagetitle'])) {
+        // TODO reimplement
+        //On récupère alors le nom de l'expéditeur et le titre pour le passer à la page suivante
+//        $_SESSION['receiver'] = $sender;
+//        $_SESSION['retitle'] = 'Re:' . $_POST['messagetitle'];
+        // On dirige ensuite l'utilisateur à la page de rédaction du message
+//        header('Location: message.php');
+//        die();
+    }
+}
+
+include 'parts/header.php';
 ?>
+<div class="container">
+    <a class="btn btn-secondary btn-lg mt-5" type="button" href="message.php">+ New message</a>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Reception box</title>
-    </head>
-    <body>
-    <div class="container">
-        <a class="btn btn-secondary btn-lg mt-5" type="button" href="message.php">+ New message</a>
-
-<?php
-    
-    $file_db = new PDO('sqlite:../databases/database.sqlite');
-    // Set errormode to exceptions
-    $file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    <?php
 
     // On récupère l'id de l'utilisateur connecté grâce à son username
     $user = $_SESSION['username'];
-    $user_query = $file_db->query("SELECT id FROM collaborators WHERE `login`='$user';")->fetch();
+    $user_query = $pdo->query("SELECT id FROM collaborators WHERE `login`='$user';")->fetch();
     $user_id = $user_query['id'];
 
     // Liste des messages adressés à l'utilisateur
-    $messages_query = $file_db->query(" SELECT * FROM 'messages' WHERE idDestinataire = {$user_id} ORDER BY time_value DESC;");
+    $messages_query = $pdo->query("SELECT * FROM messages WHERE idDestinataire = '{$user_id}' ORDER BY 'time_value' DESC;");
 
     $html = "";
     // Pour créer un id unique pour permettre à details de ne révéler qu'un seul contenu de message
     $i = 0;
-    while($row = $messages_query->fetch(PDO::FETCH_ASSOC)){
+    while ($row = $messages_query->fetch(PDO::FETCH_ASSOC)) {
         $i += 1;
-        $sender_query = $file_db->query(" SELECT login FROM collaborators WHERE `id` = '{$row['idExpediteur']}'; ")->fetch();
+        $sender_query = $pdo->query("SELECT login FROM collaborators WHERE `id` = '{$row['idExpediteur']}'; ")->fetch();
         $sender = $sender_query['login'];
 
         // Pour chaque message qui apparaît, on crée un bouton "Answer", "Delete" et surtout "Details"
-        $html .= 
-        "<div class='card m-5 w-75'>
+        $html .=
+            "<div class='card m-5 w-75'>
             <div class='card-header'>
                 <p>    
                     <div class='font-weight-bold'> 
                         <h4 class='d-flex justify-content-between align-items-center'> 
                         {$row['title']}
-                        <button class='btn btn-sm btn-dark' data-toggle='collapse' data-target='#collapse{$i}' type='button' aria-expanded='false' aria-controls='collapse{$i}'> Details </button>
+                        <button class='btn btn-sm btn-dark' data-bs-toggle='collapse' data-bs-target='#collapse{$i}' type='button' aria-expanded='false' aria-controls='collapse{$i}'> Details </button>
                         </h4> 
                     </div>
-                    <div class='text-muted'> <h6>From : {$sender} </h6> </div>
+                    <div class='text-muted'> <h6>From : {$sender}</h6></div>
                     <div> {$row['time_value']} </div>
                 </p>
             </div>
@@ -74,34 +91,8 @@
         </div>";
     }
     echo $html;
-?>
+    ?>
 
+</div>
 
-<?php
-    // Vérifie que le bouton delet a bien été pressé
-    if(isset($_POST['delete'])){
-        // Et que l'id du message existe bien
-        if(isset($_POST['messageid'])){
-            $message_id = $_POST['messageid'];
-            $file_db->exec(" DELETE FROM messages WHERE id=$message_id; ");
-            header('Location: '.$_SERVER['REQUEST_URI']);
-            die();
-        }
-    }
-    //Sinon on regarde si c'est le bouton answer
-    else if(isset($_POST['answer'])){
-        // On continue seulement si l'id du message et le titre son bien set
-        if(isset($_POST['messageid']) && isset($_POST['messagetitle'])){
-            //On récupère alors le nom de l'expéditeur et le titre pour le passer à la page suivante
-            $_SESSION['receiver'] = $sender;
-            $_SESSION['retitle'] = 'Re:' . $_POST['messagetitle'];
-            // On dirige ensuite l'utilisateur à la page de rédaction du message
-            header('Location: message.php');
-            die();
-        }
-    }
-?>
-
-    </div>
-    </body>
-</html>
+<?php include 'parts/footer.php';
