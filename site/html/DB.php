8ef70b10ec9cf2415a69489436ca471fbab82284
Gil Balsiger <gil.balsiger@gmail.com>
2020-12-30 15:30:33 +0100

    refactor(database): migrate DB.php to MySQL

--- site/html/DB.php
+++ site/html/DB.php
@@ -1,6 +1,8 @@
 <?php
-// Set default timezone
-date_default_timezone_set('UTC');
+
+use App\Database;
+
+require 'includes.php';
 
 try {
     /**************************************
@@ -8,37 +10,33 @@ try {
      * open connections                    *
      **************************************/
 
-    // Create (connect to) SQLite database in file
-    $file_db = new PDO('sqlite:../databases/database.sqlite');
-    // Set errormode to exceptions
-    $file_db->setAttribute(PDO::ATTR_ERRMODE,
-        PDO::ERRMODE_EXCEPTION);
+    $pdo = Database::getInstance()->getPdo();
 
     /**************************************
      * Create tables                       *
      **************************************/
 
     // Drop table messages from file db
-    $file_db->exec("DROP TABLE IF EXISTS collaborators");
+    $pdo->exec("DROP TABLE IF EXISTS collaborators");
 
     // Create table messages
-    $file_db->exec("CREATE TABLE IF NOT EXISTS collaborators (
-                    id INTEGER PRIMARY KEY,
-                    admin INTEGER DEFAULT 0,
-                    login TEXT NOT NULL ,
-                    password TEXT NOT NULL ,
-                    validity INTEGER DEFAULT 0)");
+    $pdo->exec("CREATE TABLE IF NOT EXISTS collaborators (
+                    id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+                    admin BOOLEAN DEFAULT FALSE,
+                    login TEXT NOT NULL,
+                    password TEXT NOT NULL,
+                    validity BOOLEAN DEFAULT FALSE)");
 
-            // Drop table messages from file db
-            $file_db->exec("DROP TABLE IF EXISTS messages");
-            // Create table messages
-            $file_db->exec("CREATE TABLE IF NOT EXISTS messages (
-                            id INTEGER PRIMARY KEY, 
-                            title TEXT, 
+    // Drop table messages from file db
+    $pdo->exec("DROP TABLE IF EXISTS messages");
+    // Create table messages
+    $pdo->exec("CREATE TABLE IF NOT EXISTS messages (
+                            id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+                            title VARCHAR(255), 
                             content TEXT, 
                             time_value DATETIME,
-                            idExpediteur INTEGER,
-                            idDestinataire INTEGER)");
+                            idExpediteur INTEGER UNSIGNED,
+                            idDestinataire INTEGER UNSIGNED)");
 
     /**************************************
      * Set initial data                    *
@@ -48,15 +46,15 @@ try {
     $user = array(
         array('admin' => 1,
             'login' => 'admin',
-            'password' => password_hash('passw0rd',   PASSWORD_BCRYPT),
+            'password' => password_hash('passw0rd', PASSWORD_BCRYPT),
             'validity' => 1),
         array('admin' => 0,
             'login' => 'Prof',
-            'password' => password_hash('password',   PASSWORD_BCRYPT),
+            'password' => password_hash('password', PASSWORD_BCRYPT),
             'validity' => 1),
         array('admin' => 0,
             'login' => 'Assistant',
-            'password' => password_hash('password',   PASSWORD_BCRYPT),
+            'password' => password_hash('password', PASSWORD_BCRYPT),
             'validity' => 1),
     );
 
@@ -82,13 +80,13 @@ try {
     // Insertion of collaborators and messages 
 
     foreach ($user as $m) {
-        $file_db->exec("INSERT INTO collaborators (admin, login, password, validity)
+        $pdo->exec("INSERT INTO collaborators (admin, login, password, validity)
                 VALUES ('{$m['admin']}', '{$m['login']}', '{$m['password']}', '{$m['validity']}')");
     }
 
     foreach ($messages as $m) {
         $formatted_time = date('Y-m-d H:i:s', $m['time_value']);
-        $file_db->exec("INSERT INTO messages (title, content, time_value, idExpediteur, idDestinataire) 
+        $pdo->exec("INSERT INTO messages (title, content, time_value, idExpediteur, idDestinataire) 
                 VALUES ('{$m['title']}',
                         '{$m['content']}', 
                         '{$formatted_time}',
@@ -96,6 +94,5 @@ try {
                         '{$m['idDestinataire']}')");
     }
 } catch (PDOException $e) {
+    die($e->getMessage());
 }
-header('Refresh: 0; URL = login.php');
-?>
