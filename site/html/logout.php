d7674a328a8560bd59577a584a134906b929e1ad
Gil Balsiger <gil.balsiger@gmail.com>
2020-12-30 15:29:51 +0100

    fix: logout the app

--- site/html/logout.php
+++ site/html/logout.php
@@ -1,11 +1,7 @@
 <?php
-// inclusion du layout de la page et de la redirection en cas de non connexion
-include("header.php");
-include('redirect.php');
-session_start();
-// on enlève toutes données de session et on redirige sur la page login
+require 'includes.php';
+
 unset($_SESSION["username"]);
 unset($_SESSION["admin"]);
-
-header('URL = login.php');
-?>
+header('Location: login.php');
+die();
