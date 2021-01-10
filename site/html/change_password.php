cd7a9a3692b8cf6a6eb74c1a7daabdc74df687e1
Gil Balsiger <gil.balsiger@gmail.com>
2021-01-10 16:54:48 +0100

    refactor: change password

--- site/html/change_password.php
+++ site/html/change_password.php
@@ -1,50 +1,83 @@
 <?php
 
+use App\Auth;
 use App\Database;
+use App\Flash;
 
 require 'includes.php';
 
-include 'parts/header.php';
 // Create (connect to) SQLite database in file
 $pdo = Database::getInstance()->getPdo();
-// inclusion du layout de la page et de la redirection en cas de non connexion
-include('redirect.php');
 
-if (!empty($_POST['password-modifier']) && !empty($_POST['password-modifier_repeat'])) {
-    $password = password_hash($_POST['password-modifier'], PASSWORD_BCRYPT);
-    $username = $_SESSION['username'];
+Auth::check();
 
-    // check si les deux mdp correspondent
-    if (password_verify($_POST['password-modifier_repeat'], $password)) {
-        try {
-            // modification dans la db
-            $query = $pdo->query("UPDATE collaborators SET password='$password' WHERE `login`='$username';");
-        } catch (Exception $e) {
+if (!empty($_POST)) {
+    if (!empty($_POST['password-old']) && !empty($_POST['password-modifier']) && !empty($_POST['password-modifier_repeat'])) {
+
+        $userId = $_SESSION['user']['id'];
+        $oldPassword = $_POST['password-old'];
+        $req = $pdo->prepare('SELECT password FROM collaborators WHERE id = ?');
+        $req->execute([$userId]);
+        $oldPasswordHash = $req->fetchColumn();
+
+        $password = $_POST['password-modifier'];
+        $password2 = $_POST['password-modifier_repeat'];
+        if (password_verify($oldPassword, $oldPasswordHash)) {
+            if ($password === $password2) {
+                // modification dans la db
+                $req = $pdo->prepare("UPDATE collaborators SET password = ? WHERE id = ?");
+                $req->execute([
+                    password_hash($password, PASSWORD_BCRYPT),
+                    $userId
+                ]);
+                Flash::success('Password updated successfully');
+                header('Location: list_messages.php');
+                die();
+            } else {
+                Flash::error('The two new passwords are not the same');
+            }
+        } else {
+            Flash::error('Incorrect old password');
         }
+        // check si les deux mdp correspondent
+
     } else {
-        // affichage message d'erreur
-        echo "<div class='m-3 d-flex align-items-center justify-content-center'>
-                    <div class='alert alert-danger'>The two password are not the same.</div>
-                </div>";
+        Flash::error('All fields must be filled');
     }
 }
+
+include 'parts/header.php';
 ?>
 
-<!-- Form for changing password -->
-<div class="container">
-    <h1>Change Password</h1>
-    <hr>
-    <form class="form-signin" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-        <div class="mb-3">
-            <label for="pass1" class="form-label">New password</label>
-            <input type="password" class="form-control" name="password-modifier" id="pass1" placeholder="Password">
+    <!-- Form for changing password -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <h1>Change Password</h1>
+                <hr>
+                <form role="form" action="change_password.php" method="post">
+                    <div class="mb-3">
+                        <label for="passold" class="form-label">Old password</label>
+                        <input type="password" class="form-control" name="password-old" id="passold"
+                               placeholder="Password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="pass1" class="form-label">New password</label>
+                        <input type="password" class="form-control" name="password-modifier" id="pass1"
+                               placeholder="Password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="pass2" class="form-label">Repeat new password</label>
+                        <input type="password" class="form-control" name="password-modifier_repeat" id="pass2"
+                               placeholder="Repeat Password">
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <button class="btn btn-primary" type="submit">Change password</button>
+                        <a href="list_messages.php">Cancel</a>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="pass2" class="form-label">Repeate new password</label>
-            <input type="password" class="form-control" name="password-modifier_repeat" id="pass2" placeholder="Repeat Password">
-        </div>
-        <button class="btn btn-primary" type="submit">Change password</button>
-    </form>
-</div>
+    </div>
 
 <?php include 'parts/footer.php';
